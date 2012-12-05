42107f66a1920fea8fbbaa01eba284cfd459b8d7
Andrew Stevens <andy@orangesix.com>
2012-12-05 13:31:48 -0600

    rename method

--- lib/yammer/client/networks.rb
+++ lib/yammer/client/networks.rb
@@ -2,7 +2,7 @@ module Yammer
   class Client
     # Defines methods related to manipulating networks
     module Networks
-      def current(options = {})
+      def current_networks(options = {})
         response = get("networks/current", options, :json)
       end
     end
